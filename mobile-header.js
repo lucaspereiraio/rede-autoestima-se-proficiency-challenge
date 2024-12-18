edac1129ec0f1410ef7e53f89bd2ab273f1e75a1
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-12-18 16:44:39 -0300

    comments

--- mobile-header.js
+++ mobile-header.js
@@ -1,32 +1,29 @@
+// MENU TOGGLE LISTENER
+
 const menuToggle = document.getElementById("menu-toggle");
 const menuClose = document.getElementById("menu-close");
 const mainMenu = document.getElementById("main-menu");
 const menuItems = document.querySelectorAll(".menu-item");
 
-// Overlay
 const overlay = document.createElement("div");
 overlay.className = "overlay";
 document.body.appendChild(overlay);
 
-// Abrir o menu
 menuToggle.addEventListener("click", () => {
   mainMenu.classList.add("active");
   overlay.classList.add("active");
 });
 
-// Fechar com o X
 menuClose.addEventListener("click", () => {
   mainMenu.classList.remove("active");
   overlay.classList.remove("active");
 });
 
-// Fechar com o overlay
 overlay.addEventListener("click", () => {
   mainMenu.classList.remove("active");
   overlay.classList.remove("active");
 });
 
-// Fechar com um item
 menuItems.forEach((item) => {
   item.addEventListener("click", () => {
     mainMenu.classList.remove("active");
